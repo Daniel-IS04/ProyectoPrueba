bd5f12edb1b77b5b824acf15302e20f09cbf7a8d
Daniel-IS04 <danieljosuecr8@gmail.com>
2025-07-10 10:07:37 -0500

    feat: create endpoints for register and login

---
+++ final_project/backend/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterViews, LoginView
+
+
+urlpatterns = [
+    path('register/', RegisterViews.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login')
+]
