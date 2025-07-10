5cf291a10a03e3230c81ad556ef665f281743b2c
Daniel-IS04 <danieljosuecr8@gmail.com>
2025-07-10 17:02:03 -0500

    fix: fix config angular for deploy vercel

---
+++ final_project/frontend/colabook/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl:    'http://localhost:8000',
+    supabaseUrl:    'https://rhifexndinjrikwlrclp.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoaWZleG5kaW5qcmlrd2xyY2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMjI0NzYsImV4cCI6MjA2NzY5ODQ3Nn0.mk-BpBJvTvzy8vNFsM1D8zknY8kqmAMNPpGUWrLm5bQ',
+};
