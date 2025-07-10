4aecb324f5be11602fea95948a2a91b444c016c5
Daniel-IS04 <danieljosuecr8@gmail.com>
2025-07-10 09:57:06 -0500

    feat: create viewe for register

--- final_project/backend/users/views.py
+++ final_project/backend/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets, status
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from .serializers import RegisterSerializer
 
 # Create your views here.
+class RegisterViews(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    
