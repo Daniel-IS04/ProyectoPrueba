14b2dc5e8eca38be7ffc33b13dff5a2cff3ffc5b
Daniel-IS04 <danieljosuecr8@gmail.com>
2025-07-10 18:02:00 -0500

    chore

--- final_project/frontend/colabook/src/app/app.ts
+++ final_project/frontend/colabook/src/app/app.ts
@@ -2,7 +2,7 @@ import { Component }       from '@angular/core';
 import { RouterOutlet }    from '@angular/router';
 import { CommonModule }    from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+// hola
 @Component({
   selector: 'app-root',
   standalone: true,
