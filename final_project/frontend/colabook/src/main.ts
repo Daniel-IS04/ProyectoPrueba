53a5fbf06bce1cedf9ea3a04c7272c881d2ad842
Daniel-IS04 <danieljosuecr8@gmail.com>
2025-07-10 19:59:51 -0500

    fix routing

--- final_project/frontend/colabook/src/main.ts
+++ final_project/frontend/colabook/src/main.ts
@@ -1,6 +1,13 @@
+// main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { App } from './app/app';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    // Aquí habilitamos hash routing:
+    provideRouter(routes, withHashLocation())
+  ]
+})
+  .catch(err => console.error(err));
